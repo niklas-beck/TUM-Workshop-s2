545fe90e533006e4a38b6d6988c0b4d1bee2fe53
Niklas Beck <Niklas.Beck@berylls.com>
2023-12-05 12:17:39 +0100

    rename basename

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "basename" {
   type        = string
   description = "The base name for all resources"
-  default     = "tumworkshop-s2-temp"
+  default     = "s2tumworkshop0"
 }
 
 variable "resource_group_name" {
