e3bb3bbd8808f74c0ade31e6a7a02be55f3c6aa5
Niklas Beck <Niklas.Beck@berylls.com>
2023-12-07 11:11:04 +0100

    change from resource manger id to id

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,7 +137,7 @@ resource "azurerm_storage_blob" "criticalblob" {
 // Provides full access to Azure Storage blob containers and data
 resource "azurerm_role_assignment" "functionToStorage" {
   #scope                = azurerm_storage_account.storage_account.id
-  scope                = azurerm_storage_container.containerpub.resource_manager_id
+  scope                = azurerm_storage_container.containerpub.id
   role_definition_name = "Storage Blob Data Owner"
   principal_id         = azurerm_linux_function_app.fxn.identity[0].principal_id
 }
